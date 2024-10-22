6bd311998babb6a9cba9aadf8431ebf1337eb43f
Artur Sentsov <sen4ik@gmail.com>
2024-10-21 21:57:18 -0700

    added python script to read dx input field from wsjtx

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ app.use(cors());
 // Path to ADI file
 const filePath = process.env.ADI_FILE_PATH || path.join('C:', 'Users', 'kn6rdd', 'AppData', 'Local', 'WSJT-X', 'wsjtx_log.adi');
 
+const dxInputLogPath = path.join(__dirname, '..', 'py', 'dx_input_log.txt');
+
 app.get('/file', (req, res) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
@@ -18,6 +20,17 @@ app.get('/file', (req, res) => {
             return res.status(500).send('Error reading file');
         }
 		console.log("Sending data");
+        res.send(data);
+    });
+});
+
+app.get('/dx_input', (req, res) => {
+    fs.readFile(dxInputLogPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading dx_input_log.txt:', err);
+            return res.status(500).send('Error reading dx_input_log.txt');
+        }
+        console.log("Sending DX input data");
         res.send(data);
     });
 });
